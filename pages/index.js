d8f70cc45491b2fd2a7c998c8efa706e973bc198
XenoZ <xenoz0096@gmail.com>
2024-07-31 18:58:18 +0530

    final push

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import { Main } from "./components/pages/main";
+import Main from "./components/pages/main";
 import { Inter } from "next/font/google";
 import Link from 'next/link';
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Main/>
+      <Main />
     </div>
   );
 }
